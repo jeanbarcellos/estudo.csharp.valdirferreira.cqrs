d17c393c7be1d98cf193e661b73a0e83cacbc8cd
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-17 22:19:40 -0300

    Implementação do Handler

---
+++ src/WebMediator/Domain/Handlers/IProductInsertHandler.cs
@@ -0,0 +1,10 @@
+﻿using WebMediator.Domain.Commands.Requests;
+using WebMediator.Domain.Commands.Responses;
+
+namespace WebMediator.Domain.Handlers
+{
+    public interface IProductInsertHandler
+    {
+        ProductResponse Handle(ProductRequest request);
+    }
+}
---
+++ src/WebMediator/Domain/Handlers/ProductInsertHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WebMediator.Domain.Commands.Requests;
+using WebMediator.Domain.Commands.Responses;
+
+namespace WebMediator.Domain.Handlers
+{
+    public class ProductInsertHandler : IRequestHandler<ProductRequest, ProductResponse>
+    {
+        public Task<ProductResponse> Handle(ProductRequest request, CancellationToken cancellationToken)
+        {
+            var result = new ProductResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Iphone XR",
+                Value = 4000,
+                Date = DateTime.Now
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+}
