acad51750725d86e2314c7911277d474112be6f5
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-17 22:19:17 -0300

    Criação da Entity

---
+++ src/WebMediator/Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace WebMediator.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Value { get; private set; }
+
+        public Product(string name, decimal value)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Value = value;
+        }
+
+    }
+}
