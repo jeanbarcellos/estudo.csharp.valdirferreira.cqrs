618d1d2f38a19e13e5e7438cdec5e309c139a674
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-17 22:19:29 -0300

    Criação dos Commands

---
+++ src/WebMediator/Domain/Commands/Requests/ProductRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using WebMediator.Domain.Commands.Responses;
+
+namespace WebMediator.Domain.Commands.Requests
+{
+    public class ProductRequest : IRequest<ProductResponse>
+    {
+        public string Name { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
---
+++ src/WebMediator/Domain/Commands/Responses/ProductResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebMediator.Domain.Commands.Responses
+{
+    public class ProductResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
