ae03441e8c9c46317ae0ac8e2d782aeb195c3e66
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-02-17 22:35:38 -0300

    Criação do controller

---
+++ src/WebMediator/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebMediator.Domain.Commands.Requests;
+using WebMediator.Domain.Commands.Responses;
+
+namespace WebMediator.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ProductResponse> Insert(
+            [FromServices] IMediator mediator,
+            [FromBody] ProductRequest request
+        )
+        {
+            return await mediator.Send(request);
+        }
+    }
+}
